4222d45f97c992ac15b4fdbe38a37925f0a5f018
Jay <jay@linuxuser.eu>
2023-10-14 18:49:10 +0200

    Adding options.lua
    With the most basic options I want.

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 -- Neovim config
+require "user.options"
---
+++ lua/user/options.lua
@@ -0,0 +1,29 @@
+local options = {
+    cmdheight = 2,
+    fileencoding = "utf-8",
+    hlsearch = true,
+    ignorecase = true,
+    pumheight = 10,
+    showtabline = 2,
+    smartcase = true,
+    smartindent = true,
+    splitbelow = true,
+    splitright = true,
+    termguicolors = true,
+    swapfile = false,
+    undofile = true,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    cursorline = true,
+    number = true,
+    relativenumber = true,
+    numberwidth = 4,
+    signcolumn = "yes",
+    scrolloff = 8,
+    colorcolumn = "80",
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
