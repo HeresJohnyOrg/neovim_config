0a363611d1eebe09867389f0c64b89ded9f4c0fc
Jay <jay@linuxuser.eu>
2023-10-15 15:47:49 +0200

    LSP config
    This was tricky, still not there 100%. Will need to tinker with config.

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -33,9 +33,27 @@ packer.init {
 
 return packer.startup(function(use)
     use "wbthomason/packer.nvim"
-    --use "nvim-lua/popup.nvim"
+    use "nvim-lua/popup.nvim"
     use "nvim-lua/plenary.nvim"
     -- Color scheme
-    --
     use {"catppuccin/nvim", as = "catppuccin"}
+
+    -- Cmp
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-nvim-lua"
+
+      -- snippets
+    use "L3MON4D3/LuaSnip" --snippet engine
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+
+    -- LSP
+    use "williamboman/mason.nvim" -- simple to use language server installer
+    use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
+    use "neovim/nvim-lspconfig" -- enable LSP
+
 end)
